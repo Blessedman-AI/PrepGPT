87711da8abd54e4b643dabca76f93ce76a4fc29f
Blessedman-AI <beemhanalabo@gmail.com>
2025-08-09 12:37:17 +0100

    added health check

--- back-end/routes/healthCheckRoute.js
+++ back-end/routes/healthCheckRoute.js
@@ -2,14 +2,12 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/health', (req, res) => {
-  res
-    .status(200)
-    .json({
-      message: 'Backend api working!',
-      status: 'ok',
-      uptime: process.uptime(),
-    });
+router.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Backend api working!',
+    status: 'ok',
+    uptime: process.uptime(),
+  });
 });
 
 export default router;
